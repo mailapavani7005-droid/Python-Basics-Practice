ba4462410cfe1145d975e5466cc1ac7caf3326b9
mailapavani7005-droid <mailapavani7005@gmail.com>
2026-09-25 09:58:12 +0530

    Update and rename Day-3-Strings.md to Day-3-Strings.py

--- Day-3-Strings.py
+++ Day-3-Strings.py
@@ -35,3 +35,31 @@ Used to access multiple characters from a string.
 ## What I Learned
 
 I learned how to access and manipulate text using strings and string methods.
+
+
+1.name = "python" 
+print(name[0])
+
+2.name="python" 
+print(name[5])
+
+3.name="python" 
+print(name[1:4])
+
+4.name=input("Enter the name:") 
+print(name.upper()) 
+
+5.name="pavani" 
+print(name.replace("p","M"))
+
+6.text = "python Java sql" 
+print(text.split())
+
+7.first="pavani" 
+last="Maila"
+print(first+""+last)
+
+
+8name = input("Enter the name:") 
+print(len(name))
+print(name.upper()) 
